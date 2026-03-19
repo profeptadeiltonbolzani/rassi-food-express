a406690abefdb7b99faa10b42a1ad9dffdd2e502
Your name <you@example.com>
2026-03-18 21:43:23 -0300

    Segundo commit.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 content-center justify-center">
+      <Text>Olá mundo!</Text>
     </View>
   );
-}
+} 
